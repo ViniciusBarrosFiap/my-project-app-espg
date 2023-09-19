c8a27da68aa3c12af5597e23d8e0a290303710c5
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-19 11:43:09 -0300

    tabela de produtos feita

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { ListaProdutosExterna } from "../components/ListaProdutosExterna";
-
+import { Link } from "react-router-dom";
 function Produtos(){
     
     document.title = "Lista de Produtos"
@@ -29,14 +29,25 @@ function Produtos(){
                             <th>NOME</th>
                             <th>DESCRIÇÃO</th>
                             <th>PREÇO</th>
+                            <th>EDITAR</th>
                         </tr>
                     </thead>
                     <tbody>
-
+                        {
+                            listaProdutosLocal.map((item, indice)=>(
+                                <tr key={indice}>
+                                    <td>{item.id}</td>
+                                    <td>{item.nome}</td>
+                                    <td>{item.descricao}</td>
+                                    <td>R${item.preco}</td>
+                                    <td><Link to={`/editar/produtos/${item.id}`}>Editar</Link></td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
                     <tfoot>
                         <tr>
-                            <td colSpan={4}>PRODUTOS INFORMÁTICOS - QTD:</td>
+                            <td colSpan={5}>PRODUTOS INFORMÁTICOS - QTD:</td>
                         </tr>
                     </tfoot>
                 </table>
