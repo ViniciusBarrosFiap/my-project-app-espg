1058170575e6408bb21cbe43dfd4f289962f73b7
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-09-19 11:29:34 -0300

    Tabela construida

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react"
 import Cabecalho from "./components/Cabecalho"
-import Section from "./components/Section"
 import Footer from "./components/Footer"
-import reactLogo from "./assets/react.svg"
 import { Outlet } from "react-router-dom"
 export default function App() {
-  
-  let reactLogoAlt = "React Logo"
   
   return (
     <>
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react"
+import { ListaProdutosExterna } from "../components/ListaProdutosExterna";
+
 function Produtos(){
+    
+    document.title = "Lista de Produtos"
+    //Declarando o useState como uma lista de objetos [{}]
+    const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+    //Estrutura que recebe a lista de produtos externa e repassa para uma lista local
+    useEffect(()=>{
+       setListaProdutosLocal(ListaProdutosExterna)
+    }, [])
+
+    // //Usando o useEffect(hook) que executa sempre baseado em um determinado elemento. Este elemento pode ser:
+    // //Um elemento, um componente, um objeto, e ou uma váriavel. Que deve ser monitorado no array de dependencias
+    // useEffect(()=>{
+    //     console.log("Este useEffect renderiza apenas quando o objeto monitorado sofre alterações")
+    // }, [counter2])
+
     return(
-        <div>Produtos</div>
+        <div>
+            <h1>Lista de Produtos</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>DESCRIÇÃO</th>
+                            <th>PREÇO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={4}>PRODUTOS INFORMÁTICOS - QTD:</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     )
 }
 
